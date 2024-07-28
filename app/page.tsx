cd430fc949d7a1d07610d030aabd4f7123b8d0ed
Makudzyn <alt4xh0@gmail.com>
2024-07-28 09:39:04 +0300

    Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 import { lusitana} from "@/app/ui/fonts";
-import {hidden} from "next/dist/lib/picocolors";
 
 export default function Page() {
   return (
